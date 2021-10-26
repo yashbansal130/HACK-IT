448a3044fa4faea77d1ef4e4c86039e0b46d8e7a
This PC <bansal.yash.130@gmail.com>
2021-10-26 17:32:01 +0530

    ui

--- app/src/main/java/com/example/hack_it/MainActivity.java
+++ app/src/main/java/com/example/hack_it/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.hack_it;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
+import com.example.hack_it.ui.WishListActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.hack_it.databinding.ActivityMainBinding;
+import com.google.android.material.internal.NavigationMenuItemView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    public DrawerLayout drawerLayout;
+    public NavigationMenuItemView analytics;
+
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        configureNavigationDrawer();
+        configureToolbar();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -32,6 +50,46 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menulist, menu);
+        return true;
+    }
+    private void configureNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.side_nav);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.nav_transaction) {
+                    Intent intent= new Intent(MainActivity.this, WishListActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch(itemId) {
+            // Android home
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            // manage other entries if you have it ...
+        }
+        return true;
     }
 
 }
--- app/src/main/java/com/example/hack_it/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hack_it/ui/home/HomeFragment.java
@@ -5,36 +5,74 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hack_it.R;
+import com.example.hack_it.RecyclerData;
+import com.example.hack_it.RecyclerViewAdapter;
 import com.example.hack_it.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private RecyclerView recyclerView;
+    private ArrayList<RecyclerData> recyclerDataArrayList;
+    View.OnClickListener listener;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        getActivity().setTitle("HACK-IT");
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        recyclerView=(RecyclerView) root.findViewById(R.id.idCourseRV);
+
+        // created new array list..
+        recyclerDataArrayList=new ArrayList<>();
+
+        // added data to array list
+        recyclerDataArrayList.add(new RecyclerData("Puma",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Jordan",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Nike",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Adidas",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Reebok",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Puma",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Jordan",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Nike",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Adidas",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Reebok",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Puma",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Jordan",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Nike",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Adidas",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Reebok",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Puma",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Jordan",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Nike",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Adidas",R.drawable.shoes));
+        recyclerDataArrayList.add(new RecyclerData("Reebok",R.drawable.shoes));
+
+        // added data from arraylist to adapter class.
+        RecyclerViewAdapter adapter=new RecyclerViewAdapter(recyclerDataArrayList,container.getContext());
+//
+        // setting grid layout manager to implement grid view.
+        // in this method '2' represents number of columns to be displayed in grid view.
+        GridLayoutManager layoutManager=new GridLayoutManager( container.getContext(),2);
+//
+        // at last set adapter to recycler view.
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
         return root;
     }
 
